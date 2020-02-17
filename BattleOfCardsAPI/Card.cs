41c3c8ba31ee71756c6c0d7bf9537cb48b806ef5
Turcsanyadam <turcsanyadam@gmail.com>
2020-02-17 14:25:30 +0100

    Add project files.

---
+++ BattleOfCardsAPI/Card.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+
+namespace BattleOfCardsAPI
+{
+    [Serializable]
+    public class Card : ISerializable
+    {
+        public string Name { get; private set; }
+        public int Coding { get; private set; }
+        public int Gaming { get; private set; }
+        public int SoftSkills { get; private set; }
+        public double CoffeeConsumption { get; private set; }
+
+        // Initializes Card
+        public Card(string name, int coding, int gaming, int softSkills, double coffeeConsumption)
+        {
+            Name = name;
+            Coding = coding;
+            Gaming = gaming;
+            SoftSkills = softSkills;
+            CoffeeConsumption = coffeeConsumption;
+        }
+        public Card()
+        {
+
+        }
+
+        // Helper method for serialization
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name);
+            info.AddValue("Coding", Coding);
+            info.AddValue("Gaming", Gaming);
+            info.AddValue("SoftSkills", SoftSkills);
+            info.AddValue("CoffeeConsumption", CoffeeConsumption);
+
+        }
+
+        // Helper method for serialization
+        public Card(SerializationInfo info, StreamingContext context)
+        {
+            Name = (string)info.GetValue("Name", typeof(string));
+            Coding = (int)info.GetValue("Coding", typeof(int));
+            Gaming = (int)info.GetValue("Gaming", typeof(int));
+            SoftSkills = (int)info.GetValue("SoftSkills", typeof(int));
+            CoffeeConsumption = (double)info.GetValue("CoffeeConsumption", typeof(double));
+        }
+    }
+}
---
+++ BattleOfCardsAPI/Initializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace BattleOfCardsAPI
+{
+    class Initializer
+    {
+        private string filePathCardGame = @"..\\..\\..\\CardGame.xml";
+        private List<Card> listOfCards = new List<Card>();
+
+        // Deserializes Cards from XML
+        public void DeserializerCards()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Card>));
+
+            if (File.Exists(filePathCardGame) && new FileInfo(filePathCardGame).Length > 0)
+            {
+                using (FileStream fs = File.OpenRead(filePathCardGame))
+                {
+                    listOfCards = (List<Card>)serializer.Deserialize(fs);
+                }
+            }
+
+        }
+    }
+}
